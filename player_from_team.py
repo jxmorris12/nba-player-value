e9aba38bf328e352ba283816ebde2b6cc58427a6
Jack Morris <jxmorris12@gmail.com>
2016-07-05 23:58:23 -0400

    init commit

---
+++ player_from_team.py
@@ -0,0 +1,5 @@
+from soup_utils import getSoupFromURL
+
+# sample search
+  # http://www.basketball-reference.com/play-index/psl_finder.cgi?request=1&type=totals&lg_id=NBA&is_playoffs=N
+  #      &year_min=2016&year_max=2016&franch_id=CLE&age_min=0&age_max=99&height_min=0&height_max=99&order_by=ws
